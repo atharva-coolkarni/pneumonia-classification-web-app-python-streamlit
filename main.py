a58b7f2de05fc8e9a15e7fab623a19b5a4415203
Error404Occured <kulkarni.atharva.18.05.2004@gmail.com>
2025-01-04 21:29:59 +0530

    code added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from keras.models import load_model
 from PIL import Image
-from util import classify
+import numpy as np
+
+from util import classify, set_background
+
+set_background('./bgs/bg5.png')
 
 # Set title
 st.title("Pneumonia classification")
@@ -25,7 +29,9 @@ if file is not None:
     image = Image.open(file).convert("RGB")
     st.image(image, use_column_width=True)
 
-# classify image
-classify(image, model, class_names)
+    # classify image
+    class_name, conf_score = classify(image, model, class_names)
 
-# write classification
+    # write classification
+    st.write("## {}".format(class_name))
+    st.write("### score: {}".format(conf_score))
